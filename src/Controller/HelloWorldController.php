061ef7bd1df3c00c7ccfb57601b0d3871b4cc8bf
Sandra <sandra.grillon@gmail.com>
2019-12-27 16:46:04 +0100

    Hello John

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -7,13 +7,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloWorldController extends AbstractController
 {
-    /**
-     * @Route("/hello/world", name="hello_world")
+     /**
+     * @Route("/hello/{name}", name="hello_world") //add this comment to annotations
      */
-    public function index()
+    public function index($name="World")
     {
         return $this->render('hello_world/index.html.twig', [
             'controller_name' => 'HelloWorldController',
+            'name'            => $name
         ]);
     }
 }
