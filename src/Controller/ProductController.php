ce3ad675dd1105ce7069c2b6c35ca140774fc3fc
Sandra <sandra.grillon@gmail.com>
2019-12-27 23:07:44 +0100

    Part 2 with forms

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product")
+     */
+    public function index()
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+    /**
+     * @Route("/product/add", name="product.add")
+     */
+    public function add(Request $request)
+    {
+      $form = $this->createFormBuilder()
+        ->add("name", TextType::class)
+        ->add("releaseOn", DateType::class, [
+                "widget" => "single_text"
+              ])
+        ->add("save", SubmitType::class, ["label" => "create Product"])
+        ->getForm();
+
+      return $this->render('product/add.html.twig', [
+         'controller_name' => 'ProductController',
+         'form'            => $form->createView()
+      ]);
+    }
+}
