17e38722beeb17fac73a500c807deb0e549e1245
Sandra <sandra.grillon@gmail.com>
2019-12-28 15:23:17 +0100

    Doctrine : Entity

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Task;
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -22,10 +23,31 @@ class ProductController extends AbstractController
             'controller_name' => 'ProductController',
         ]);
     }
+
+    /**
+     * @Route("/product/all", name="product.all")
+     */
+    public function all()
+    {
+      $em       = $this->getDoctrine()->getManager();
+      $products = $em->getRepository(Product::class)->findAll();
+
+      return $this->render('product/all.html.twig', ['products' => $products]);
+    }
+
+    /**
+     * @Route("/product/show/{product}", name="product.show")
+     */
+    public function show(Product $product)
+    {
+      return $this->render('product/show.html.twig', [
+      ]);
+    }
+
     /**
      * @Route("/product/add", name="product.add")
      */
-    public function add(Request $request)
+    public function add(Product $product)
     {
       $form = $this->createFormBuilder()
         ->add("name", TextType::class)
@@ -35,16 +57,41 @@ class ProductController extends AbstractController
         ->add("save", SubmitType::class, ["label" => "create Product"])
         ->getForm();
 
-      $result = [];
-      $form->handleRequest($request);
+      // $result = [];
+      $form->handleRequest($product);
       if ($form->isSubmitted() && $form->isValid()) {
-        $result = $form->getData();
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($product);
+        $em->flush();
+        return $this->redirectToRoute("product.all");
       }
 
       return $this->render('product/add.html.twig', [
-         'controller_name' => 'ProductController',
          'form_content'    => $form->createView(),
-         'result'          => $result,
+      ]);
+      // return ['form_content' => $form->createView()];
+    }
+
+    /**
+     * @Route("/product/update/{product}", name="product.update")
+     */
+    public function update(Product $product)
+    {
+      return $this->render('product/update.html.twig', [
       ]);
     }
+
+    /**
+     * @Route("/product/delete/{product}", name="product.delete")
+     */
+    public function delete(Product $product)
+    {
+      $em = $this->getDoctrine()->getManager();
+      $em->remove($product);
+      $em->flush();
+
+      return $this->redirectToRoute("product.all");
+    }
+
 }
